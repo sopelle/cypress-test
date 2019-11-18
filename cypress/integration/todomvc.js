e5000b6aacbaca29c1a8fb4e0355ba6d02cf4072
Sophie Pelle <sophie.pelle@jobteaser.com>
2019-11-18 11:21:11 +0100

    Update backbone test

--- cypress/integration/todomvc.js
+++ cypress/integration/todomvc.js
@@ -2,4 +2,7 @@ describe('TODO MVC', function() {
   it('Visits todomvc homepage', function() {
     cy.visit('http://todomvc.com/')
   })
+  it('Choose the backbone.js framework', function() {
+  	cy.get('body > div.container > div:nth-child(2) > div.col-md-8 > iron-pages > div:nth-child(1) > ul > li:nth-child(1) > a').click({ force: true })
+  })
 })
